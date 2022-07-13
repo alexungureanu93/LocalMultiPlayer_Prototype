3f96fd890fbcabaf14ee93e040bab7b277067232
HellHunter <alex.ungureanu16@yahoo.ro>
2022-07-13 21:26:46 +0300

    Import Input System package Update Player code to work with the new Input System Add new variable called velocity to store the callback context that is further used in the move code instead on Input.GetAxisRaw Add Input System component and update it with actions and move method Configure actions to work with gamepad and controller Enable local multiplayer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed, jumpForce;
     public Rigidbody2D rigidBody2D;
+    private float velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +18,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rigidBody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rigidBody2D.velocity.y);
+        rigidBody2D.velocity = new Vector2(velocity * moveSpeed, rigidBody2D.velocity.y);
 
-        if (Input.GetButtonDown("Jump")) 
-        {
-            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpForce);
-        }
+        //if (Input.GetButtonDown("Jump")) 
+        //{
+        //    rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpForce);
+        //}
+    }
+
+    public void Move(InputAction.CallbackContext context) 
+    {
+        velocity = context.ReadValue<Vector2>().x;
     }
 }
