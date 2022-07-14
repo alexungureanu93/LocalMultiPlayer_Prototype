11fd95f699a1080696a82d121921ef0d72c89ef0
HellHunter <alex.ungureanu16@yahoo.ro>
2022-07-14 17:50:28 +0300

    create new Layers to enable checking if the player is grounded or not enable the jump method on the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private const float groundCheckRadius = .2f;
+    private const float reduceJumpForce = .5f;
+    private float velocity;
+    private bool isGrounded;
+
     public float moveSpeed, jumpForce;
     public Rigidbody2D rigidBody2D;
-    private float velocity;
+    public Transform groundCheckPoint;
+    public LayerMask groundLayerMask;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +24,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayerMask);
         rigidBody2D.velocity = new Vector2(velocity * moveSpeed, rigidBody2D.velocity.y);
 
         //if (Input.GetButtonDown("Jump")) 
@@ -29,5 +36,17 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context) 
     {
         velocity = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context) 
+    {
+        if (context.started && isGrounded)
+        {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpForce);
+        }
+        if(!isGrounded && context.canceled && rigidBody2D.velocity.y>0) 
+        {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, rigidBody2D.velocity.y * reduceJumpForce);
+        }
     }
 }
