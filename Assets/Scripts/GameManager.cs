fbe402e203b5020a30aed427deeb3fe847ff0b1e
HellHunter <alex.ungureanu16@yahoo.ro>
2022-07-20 18:14:08 +0300

    Change Collision matrix to avoid players getting stuck Add a limit to player count to 4 Add a Game Manager to be responsible of Players spawn limit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int maxPlayers;
+    public List<PlayerController> activePlayers = new List<PlayerController>();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddPlayer(PlayerController newPlayer) 
+    {
+        if (activePlayers.Count < maxPlayers) 
+        { 
+            activePlayers.Add(newPlayer);
+        }
+        else 
+        {
+            Destroy(newPlayer.gameObject);
+        }
+    }
+}
